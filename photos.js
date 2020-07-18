9824883afd65ba63ff1c0809894f3a8ce485d45c
Luke <lukeabennetz@gmail.com>
2020-07-18 16:14:53 +0100

    Gallery update and new photos

---
+++ photos.js
@@ -0,0 +1,106 @@
+const photos = [
+    {
+        "link": "IMG_6003",
+        "feature": true,
+        "description": "Wandering through the center of Bristol can reveal some beautiful locations. There are so many moving parts within the city, and it can sometimes be overwhelming to try and capture all of it within a single shot. The obscured roads or unnamed side streets can sometimes strech your skills and pull you into fresh territory with your camera, but that doesn't mean the more public facing architecture should be dismissed.",
+        "location": "City Centre, Bristol"
+    },
+    {
+        "link": "DSC08861",
+        "feature": true,
+        "description": "There are so many fantastic viewpoints in the Cotswolds; it's quite hard to take it all in and can prove to be quite overwhelming to capture. That said, it was such a beautiful place to just stop and pause.",
+        "location": "William Tyndale Monument, Cotswolds"
+    },
+    {
+        "link": "DSC08662",
+        "feature": true,
+        "description": "While driving along the country roads in the Cotswolds on my way to another photo locatin, I happened to stumble upon this picturesque hut in the middle of a farmers field. Only had a short time to get this before the tree line cut off the light from the shot.",
+        "location": "Drakestone House, Cotswolds"
+    },
+    {
+        "link": "DSC07502",
+        "feature": true,
+        "description": "All the cleaver shapes and patterns that you find in nature, never ceases to amaze me. Caught this while on a quick walk through a local nature reserve in some woods.",
+        "location": "Three Brooks Nature Reserve, Bristol"
+    },
+    {
+        "link": "DSC09074",
+        "feature": false,
+    },
+    {
+        "link": "IMG_9856", 
+        "feature": true,
+        "description": "During the summers spent at home I would work alongside my dad. Sometimes we would finish late into the day and travel home during the evening, but clearly this was a day was worth staying at work late.", 
+        "location": "South Devon"
+    },
+    {
+        "link": "IMG_9239", 
+        "feature": true,
+        "description": "I captured this when hiking up a mountain in Poland with my partner and other family members. It was near the beginning of hike just after emerging from the woods that we first saw the first of many peaks that we would eventually be climbing.", 
+        "location": "Zakopane, Poland"
+    },
+    {
+        "link": "IMG_9194",
+        "feature": true,
+        "description": "Coming up to the mid section of our hike up the mountain, we saw these smaller huts. With the peaks just poking into the clouds, I couldn't help but get a shot of one.",
+        "location": "Zakopane, Poland"
+    },
+    {
+        "link": "IMG_8878", 
+        "feature": true,
+        "description": "Between April and July, puffins come in to nest and raise their young, using the sheer cliff faces of Lundy Island as a safe space away from preditors.", 
+        "location": "Lundy Island, England"
+    },
+    {
+        "link": "IMG_8362", 
+        "feature": true,
+        "description": "After disembarking from the boat that takes you over to Lundy, there is a short walk up onto the top of the island. As we climbed, you could see the mist rolling in over the fishing boats that leads out to a blank sea.", 
+        "location": "Lundy Island, England"
+    },
+    {
+        "link": "IMG_7359", 
+        "feature": true,
+        "description": "There is something to be said about using only black and white to capture your subject. It provides a unique opportunity to go back to the basics of photography and lets the composition do the talking.", 
+        "location": "Hoe, Plymouth"
+    },
+    {
+        "link": "IMG_6868",
+        "feature": false, 
+    },
+    {
+        "link": "IMG_9470", 
+        "feature": false,
+    },
+    {
+        "link": "IMG_6021", 
+        "feature": false,
+    },
+    {
+        "link": "IMG_5874", 
+        "feature": false,
+    },
+    {
+        "link": "IMG_5222", 
+        "feature": false,
+    },
+    {
+        "link": "IMG_3449", 
+        "feature": false,
+    },
+    {
+        "link": "IMG_3303", 
+        "feature": false,
+    },
+    {
+        "link": "IMG_2416", 
+        "feature": false,
+    },
+    {
+        "link": "lamppost_walk", 
+        "feature": false,
+    },
+    {
+        "link": "fireworks17", 
+        "feature": false,
+    },
+];
