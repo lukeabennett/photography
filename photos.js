fd417b2e67b7fff755ba5cedb7f2e5cb02ee35ca
Luke <lukeabennetz@gmail.com>
2020-08-18 22:09:03 +0100

    description changes

--- photos.js
+++ photos.js
@@ -2,13 +2,13 @@ const photos = [
     {
         "link": "IMG_6003",
         "feature": true,
-        "description": "Wandering through the center of Bristol can reveal some beautiful locations. There are so many moving parts within the city, and it can sometimes be overwhelming to try and capture all of it within a single shot. The obscured roads or unnamed side streets can sometimes strech your skills and pull you into fresh territory with your camera, but that doesn't mean the more public facing architecture should be dismissed.",
+        "description": "Wandering through the center of Bristol can reveal some beautiful locations. There are so many moving parts within the city, and it can sometimes be overwhelming to try and capture all of it within a single frame. The obscured roads or unnamed side streets can sometimes strech your skills and pull you into fresh territory with your camera, but that doesn't mean the more public facing architecture should be dismissed.",
         "location": "City Centre, Bristol"
     },
     {
         "link": "DSC08861",
         "feature": true,
-        "description": "There are so many fantastic viewpoints in the Cotswolds; it's quite hard to take it all in and can prove to be quite overwhelming to capture. That said, it was such a beautiful place to just stop and pause.",
+        "description": "There are so many fantastic viewpoints in the Cotswolds; it's quite nice to stop and take it all in.",
         "location": "William Tyndale Monument, Cotswolds"
     },
     {
@@ -20,7 +20,7 @@ const photos = [
     {
         "link": "DSC07502",
         "feature": true,
-        "description": "All the cleaver shapes and patterns that you find in nature, never ceases to amaze me. Caught this while on a quick walk through a local nature reserve in some woods.",
+        "description": "All the cleaver shapes and patterns found in nature never cease to amaze me. Caught this while on a quick walk through a local nature reserve in the woods.",
         "location": "Three Brooks Nature Reserve, Bristol"
     },
     {
@@ -30,37 +30,37 @@ const photos = [
     {
         "link": "IMG_9856", 
         "feature": true,
-        "description": "During the summers spent at home I would work alongside my dad. Sometimes we would finish late into the day and travel home during the evening, but clearly this was a day was worth staying at work late.", 
+        "description": "During the summers spent at home I would work alongside my dad. Sometimes we would finish late into the day and travel home during the evening; clearly this was a day worth staying late at work.", 
         "location": "South Devon"
     },
     {
         "link": "IMG_9239", 
         "feature": true,
-        "description": "I captured this when hiking up a mountain in Poland with my partner and other family members. It was near the beginning of hike just after emerging from the woods that we first saw the first of many peaks that we would eventually be climbing.", 
+        "description": "I captured this while hiking up a mountain in Poland with my partner and other family members. It was near the beginning of hike just after emerging from the woods that we saw the first of many peaks we would eventually be climbing.", 
         "location": "Zakopane, Poland"
     },
     {
         "link": "IMG_9194",
         "feature": true,
-        "description": "Coming up to the mid section of our hike up the mountain, we saw these smaller huts. With the peaks just poking into the clouds, I couldn't help but get a shot of one.",
+        "description": "Coming up to the mid section of our hike up the mountain, I loved this little hut against the backdrop. With the peaks just touching the clouds, I couldn't help but get a shot.",
         "location": "Zakopane, Poland"
     },
     {
         "link": "IMG_8878", 
         "feature": true,
-        "description": "Between April and July, puffins come in to nest and raise their young, using the sheer cliff faces of Lundy Island as a safe space away from preditors.", 
+        "description": "Between April and July, puffins come in to nest and raise their young using the sheer cliff faces of Lundy Island as a safe space away from preditors.", 
         "location": "Lundy Island, England"
     },
     {
         "link": "IMG_8362", 
         "feature": true,
-        "description": "After disembarking from the boat that takes you over to Lundy, there is a short walk up onto the top of the island. As we climbed, you could see the mist rolling in over the fishing boats that leads out to a blank sea.", 
+        "description": "After disembarking from the Lundy boat, there is was short walk up onto the top of the island. As we climbed, we could see the mist rolling in over the fishing boats.", 
         "location": "Lundy Island, England"
     },
     {
         "link": "IMG_7359", 
         "feature": true,
-        "description": "There is something to be said about using only black and white to capture your subject. It provides a unique opportunity to go back to the basics of photography and lets the composition do the talking.", 
+        "description": "There is something to be said about using only black and white to capture your subject. It provides a unique opportunity to go back to the basics of photography.", 
         "location": "Hoe, Plymouth"
     },
     {
